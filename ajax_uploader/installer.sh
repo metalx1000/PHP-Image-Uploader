7b3e96b250c48eea67d150e8b6ad9850e9e5188a
metalx1000 <github@filmsbykris.com>
2017-07-21 13:44:28 -0400

    added install script for permissions

--- ajax_uploader/installer.sh
+++ ajax_uploader/installer.sh
@@ -1,23 +1,5 @@
 #!/bin/bash
 
-browser="google-chrome"
-editor="vim"
-
-if [ "$1" = "" ]
-then
-  echo "What do you want to call your project?"
-  read dir
-else
-  dir="$1"
-fi
-
-echo "Creating Directory for $dir"
-mkdir $dir
-cd $dir
-
-echo "Getting Package..."
-wget "https://github.com/metalx1000/PHP-Image-Uploader/blob/master/ajax_uploader/packaged.zip?raw=true" -O "package.zip"
-unzip package.zip
-rm package.zip
-
-$browser localhost
+mkdir -p uploads/photos
+chmod 777 uploads
+chmod 777 uploads/photos
